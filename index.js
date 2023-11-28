c254fa17809c24e408ca149947d01fe3732f7560
pdLeal <lealpd@outlook.com>
2023-11-28 10:56:11 -0300

    add: sale animation (still has work to do)

---
+++ index.js
@@ -0,0 +1,20 @@
+const scrollers = document.querySelectorAll(".scroller");
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    addAnimation();
+}
+
+function addAnimation() {
+    scrollers.forEach(scroller => {
+        scroller.setAttribute("data-animated", true);
+
+        const innerScroller = scroller.querySelector(".inner-scroller");
+        const scrollerContent = Array.from(innerScroller.children);
+
+        scrollerContent.forEach(item => {
+            const duplicatedItem = item.cloneNode(true);
+            duplicatedItem.setAttribute("aria-hidden", true);
+            innerScroller.appendChild(duplicatedItem);
+        });
+    });
+}
