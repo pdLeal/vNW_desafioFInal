e288ce30c4db87254c71bc9adb6bb5e2d64ada0e
pdLeal <lealpd@outlook.com>
2023-11-30 15:01:15 -0300

    pré-testes de search-bar

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// BANNER SCROLL
 const scrollers = document.querySelectorAll(".scroller");
 const outer1 = document.querySelector(".scroller");
 const inner1 = document.querySelector(".inner-scroller");
@@ -26,3 +27,38 @@ function addAnimation() {
 window.addEventListener("resize", function () {
     if (inner1.offsetWidth < (2 * outer1.offsetWidth)) addAnimation();
 });
+
+// SEARCH BUTTON
+const h4s = document.querySelectorAll("h4");
+const searchInput = document.querySelector(".search-input");
+const suggestions = document.querySelector(".suggestions");
+const products = [];
+
+h4s.forEach(product => {
+    products.push(product.textContent);
+});
+
+function findMatches(wordToMatch, products) {
+    return products.filter(product => {
+        const regex = new RegExp(wordToMatch, "gi");
+        return product.match(regex);
+    });
+}
+
+function displayMatches() {
+    const matchArray = findMatches(this.value, products);
+    const html = matchArray.map(product => {
+        const regex = new RegExp(this.value, "gi");
+        const productName = product.replace(regex, `<span class="hl">${this.value}</span>`);
+
+        return `
+            <li>
+                ${productName}
+            </li>
+        `
+    }).join("");
+
+    suggestions.innerHTML = html;
+}
+
+searchInput.addEventListener("input", displayMatches);
